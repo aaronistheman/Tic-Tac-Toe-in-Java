78b88849216ffd9bfacea242c2db1d1f9d2ffed3
aaronistheman <aarons.7007@gmail.com>
2015-01-01 11:33:39 -0800

    Added an object for each square and made their values be displayed on the board

--- src/Application.java
+++ src/Application.java
@@ -9,23 +9,35 @@
  
 public class Application 
 {
+  // *******************  Static constants  *******************
+  
+  static final char x = 'x';
+  static final char o = 'o';
+  static final char blank = ' ';
+  
   // ******************  Instance variables  ******************
   
-  
+  char a1, a2, a3, b1, b2, b3, c1, c2, c3;
   
   // *********************  Constructors  *********************
   
   public Application()
   {
-    System.out.println("Application::Application()");
+    a1 = blank;
+    a2 = blank;
+    a3 = blank;
+    b1 = blank;
+    b2 = blank;
+    b3 = blank;
+    c1 = blank;
+    c2 = blank;
+    c3 = blank;
   }
   
   // ********************  Public methods  ********************
   
   public void run()
   {
-    System.out.println("Application::run()");
-    
     displayBoard();
   }
   
@@ -33,12 +45,13 @@ public class Application
   
   private void displayBoard()
   {
-    System.out.println("  |  |  ");
-    System.out.println("__|__|__");
-    System.out.println("  |  |  ");
-    System.out.println("__|__|__");
-    System.out.println("  |  |  ");
-    System.out.println("  |  |  ");
+    System.out.println("   " + a1 + "| " + a2 + "| " + a3);
+    System.out.println("a __|__|__");
+    System.out.println("   " + b1 + "| " + b2 + "| " + b3);
+    System.out.println("b __|__|__");
+    System.out.println("   " + c1 + "| " + c2 + "| " + c3);
+    System.out.println("c   |  |  ");
+    System.out.println("  1  2  3 ");
   }
   
 }
